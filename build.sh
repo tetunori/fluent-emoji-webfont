70c4c4d02405fa83b5aa607f480e89bd7d5eebd9
tetunori <tetunori@gmail.com>
2024-12-25 17:14:36 +0900

    Brushed up scripts

--- build.sh
+++ build.sh
@@ -8,10 +8,12 @@ rm -rf venv build
 
 # Create clean Python environment.
 python -m venv --upgrade-deps venv
+
 # source venv/bin/activate
 source venv/Scripts/activate # For Windows
+
 pip install nanoemoji
-pip install brotli
+pip install brotli # Add for conversion of woff2
 
 # Prepare the emoji SVG files for nanoemoji by:
 # 1) Renaming them from 'glyph_name.svg' to 'emoji_uxxxx.svg'.
@@ -30,6 +32,9 @@ git apply --directory venv/Lib/site-packages/nanoemoji nanoemoji.patch
 # Build the ttf font file using the COLR1 format for the colored glyphs.
 pushd build
 
+CSSFILENAME="fluentColorEmoji.css"
+echo -n >| ${CSSFILENAME}
+
 # for i in {1..149} ; do
 for i in {1..3} ; do
   echo ${i}
@@ -43,13 +48,37 @@ for i in {1..3} ; do
   # echo $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20)
 
   nanoemoji --color_format glyf_colr_1 --family 'Fluent Color Emoji' --output_file ${FILENAME} $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20)
+  
   pushd build
   maximum_color --bitmaps --output_file ${FILENAME} ${FILENAME}
   mv build/${FILENAME} ..
   popd
+  
+  # WOFFFILENAME="FluentColorEmoji${FILENUM}.woff"
+  # fonttools ttLib ${FILENAME} --flavor woff -o ${WOFFFILENAME}
   WOFF2FILENAME="FluentColorEmoji${FILENUM}.woff2"
-  fonttools ttLib ${FILENAME} --flavor woff2 -o ${WOFF2FILENAME}
+  fonttools ttLib ${FILENAME} --flavor woff2 -o ../dist/${WOFF2FILENAME}
+
+  UNICODERANGE=$( \
+    echo $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20) \
+    | sed "s/.\/emoji_u/U+/g" \
+    | sed "s/_/, U+/g" \
+    | sed "s/.svg/,/g" \
+    | sed "s/U+0*/U+/g" \
+    | sed -z "s/,\n/;\n/g" \
+  )
+
+  echo "
+  /* [${FILENUM}] */
+  @font-face {
+    font-family: 'fluentcojloremoji';
+    font-display: swap;
+    src: url('./dist/${WOFF2FILENAME}') format('woff2');
+    unicode-range: ${UNICODERANGE}
+  }" >> ${CSSFILENAME}
 done
+
+mv ${CSSFILENAME} ../dist 
 
 # Move the final font file to the build directory and clean up.
 rm -rf build *.svg
--- buildtest.sh
+++ buildtest.sh
@@ -16,16 +16,25 @@ for i in {1..3} ; do
   # echo $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20)
 
   nanoemoji --color_format glyf_colr_1 --family 'Fluent Color Emoji' --output_file ${FILENAME} $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20)
+  
   pushd build
   maximum_color --bitmaps --output_file ${FILENAME} ${FILENAME}
   mv build/${FILENAME} ..
   popd
+  
   # WOFFFILENAME="FluentColorEmoji${FILENUM}.woff"
   # fonttools ttLib ${FILENAME} --flavor woff -o ${WOFFFILENAME}
   WOFF2FILENAME="FluentColorEmoji${FILENUM}.woff2"
   fonttools ttLib ${FILENAME} --flavor woff2 -o ../dist/${WOFF2FILENAME}
 
-  UNICODERANGE=$(echo $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20) | sed "s/.\/emoji_u/U+/g" | sed "s/_/, U+/g" | sed "s/.svg/,/g" | sed "s/U+0*/U+/g" | sed -z "s/,\n/;\n/g")
+  UNICODERANGE=$( \
+    echo $(find -maxdepth 1 -name '*.svg' | head -n $((20*i)) | tail -n 20) \
+    | sed "s/.\/emoji_u/U+/g" \
+    | sed "s/_/, U+/g" \
+    | sed "s/.svg/,/g" \
+    | sed "s/U+0*/U+/g" \
+    | sed -z "s/,\n/;\n/g" \
+  )
 
   echo "
   /* [${FILENUM}] */
@@ -35,7 +44,6 @@ for i in {1..3} ; do
     src: url('./dist/${WOFF2FILENAME}') format('woff2');
     unicode-range: ${UNICODERANGE}
   }" >> ${CSSFILENAME}
-
 done
 
 mv ${CSSFILENAME} ../dist 
