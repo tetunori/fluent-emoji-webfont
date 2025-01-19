a024a4776953e4b808a6791a49d2e5d3dc88aeee
tetunori <tetunori@gmail.com>
2025-01-19 20:08:49 +0900

    Update build_ttf_separate02.sh

--- build_ttf_separate02.sh
+++ build_ttf_separate02.sh
@@ -27,12 +27,6 @@ fi
 pip install nanoemoji
 pip install brotli # Add for conversion of woff2
 
-if [ -d venv/Lib/site-packages/nanoemoji ]; then
-  git apply --directory venv/Lib/site-packages/nanoemoji nanoemoji.patch # For Windows
-else
-  git apply --directory venv/lib/*/site-packages/nanoemoji nanoemoji.patch # For Mac, Linux
-fi
-
 pushd build
 TTFFILENAME=$(echo "FluentEmoji${FONTTYPE}.ttf" | sed 's/ //g')
 pushd build
