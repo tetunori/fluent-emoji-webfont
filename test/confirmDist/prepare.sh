daaf1801f915f515d219529dbb1f8c54824f195f
tetunori <tetunori@gmail.com>
2025-02-08 13:35:17 +0900

    Add ttf test

--- test/confirmDist/prepare.sh
+++ test/confirmDist/prepare.sh
@@ -3,11 +3,18 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 cd $SCRIPT_DIR
 
-mkdir ./tmp
+mkdir ./tmp-woff2
+mkdir ./tmp-ttf
 
-cp -r ../../sample/* ./tmp/
-cp ../../dist/*.css ./tmp/
+cp -r ../../sample/* ./tmp-woff2/
+cp -r ../../sample/* ./tmp-ttf/
+cp ../../dist/*.css ./tmp-woff2/
+cp ./ttf/*.css ./tmp-ttf/
 
-cd ./tmp
+cd ./tmp-woff2
 find -name "style.css" | xargs sed -i "s/https:\/\/tetunori\.github\.io\/fluent-emoji-webfont\/dist/\.\./g"
 find -name "*.css" | xargs sed -i "s/https:\/\/tetunori\.github\.io\/fluent-emoji-webfont//g"
+cd ..
+
+cd ./tmp-ttf
+find -name "style.css" | xargs sed -i "s/https:\/\/tetunori\.github\.io\/fluent-emoji-webfont\/dist/\.\./g"
